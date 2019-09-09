980f6e5bc92ad81541e85fa73e70de751b8ab9ac
rajaishwary <rajaishwary@gmail.com>
2019-09-09 06:47:03 +0530

    update tiles for trailer

--- src/Containers/Trailers/index.js
+++ src/Containers/Trailers/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import {
-  withRouter
-} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { getTrailers } from "../../modules/trailers";
 import { Button } from "../../Components";
-import './styles.scss';
+import "./styles.scss";
 
 class Trailers extends React.Component {
   constructor() {
@@ -16,14 +14,26 @@ class Trailers extends React.Component {
     this.props.dispatch(getTrailers());
   }
 
+  get trailers() {
+    return !!this.props.trailers ? Object.values(this.props.trailers) : [];
+  }
+
   render() {
     return (
       <div className="main-container trailers">
         <div className="filters-header">
-          <Button>{'Comming Soon'}</Button>
-          <Button>{'Now Showing'}</Button>
+          <Button>{"Comming Soon"}</Button>
+          <Button>{"Now Showing"}</Button>
         </div>
-        {'Trailers'}
+        <div className="applied-filters">{"filters applied"}</div>
+        <div className="trailers-container">
+        {this.trailers.length
+          ? this.trailers.map(trailer => (
+              <div key={trailer.EventCode} className="trailer">{trailer.EventTitle}</div>
+            ))
+          : null}
+        </div>
+
       </div>
     );
   }
@@ -32,7 +42,7 @@ class Trailers extends React.Component {
 function mapStateToProps({ trailers }) {
   return {
     trailers: trailers.trailers,
-    languages: trailers.languages,
+    languages: trailers.languages
   };
 }
 
