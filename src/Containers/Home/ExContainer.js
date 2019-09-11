af1f1c96b4ab300ae666830e9090f57840e28880
rajaishwary <rajaishwary@gmail.com>
2019-09-11 18:31:15 +0530

    add instructions

--- src/Containers/Home/ExContainer.js
+++ src/Containers/Home/ExContainer.js
@@ -22,6 +22,11 @@ export default class ExContainer extends React.Component {
     const { error: { dupsInNums, dupsInRange, result } } = this.state;
     return (
       <div className="ex-container">
+        <div className="instructions">
+          <div>{"Example"}</div>
+          <div>{"Input: 7000, 7001, 7002, 7005, 7005-7010, 7011"}</div>
+          <div>{"Result: 7000, 7001, 7002, 7005, 7006, 7007, 7008, 7009, 7010, 7011"}</div>
+        </div>
         <React.Fragment>
           <input className="bar" value={this.state.value} onChange={event => this.handleChange(event)} />
           {dupsInNums.length || Object.keys(dupsInRange).length || result.length ? (<React.Fragment>
@@ -29,7 +34,7 @@ export default class ExContainer extends React.Component {
             <div>Duplicates in range: {Object.keys(dupsInRange).map(range => dupsInRange[range].join(", ")) || "--"}</div>
             <div>Result: {result.join(", ")}</div>
           </React.Fragment>) : null}
-          <Button className="bar">{"GET RESULTS"}</Button>
+          <Button className="bar btn">{"GET RESULTS"}</Button>
         </React.Fragment>
       </div>
     );
